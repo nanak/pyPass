1c45985b58022e4d1f05f334034620b3902198d3
Nanak Tattyrek <nanak@tattyrek.at>
2013-02-10 18:51:14 +0100

    added input function

--- pyPass.py
+++ pyPass.py
@@ -5,6 +5,7 @@ generates unique passwords that are easily reproducible
 
 from subprocess import call, Popen, PIPE
 import hashlib
+import getpass
 
 
 def getUUID():
@@ -31,8 +32,24 @@ def hashSHA1(prm):
     sha.update(senc)
     return sha.hexdigest()
 
-prt = hashSHA1("Test")
-print(prt)
+
+def inp():
+    keyw = str(input("Enter Keyword: "))
+    while 1:
+        print("Enter Master Password")
+        master1 = str(getpass.getpass())
+        print("Enter Master Password again")
+        master2 = str(getpass.getpass())
+        if master1 == master2:
+            key_and_master = keyw + master1
+            break
+        else:
+            print("Passwords did not match, try again")
+            pass
+    return key_and_master
+
+passw = hashSHA1(inp())
+print(passw)
 
 # splited = str.partition('\n')
 # print [splited[0]]
