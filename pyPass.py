ac98dfb77eae84635bf6132e29e2fd05bc0a5252
Nanak Tattyrek <nanak@tattyrek.at>
2013-02-11 13:39:11 +0100

    removed getUUID (bad idea for password generation)

--- pyPass.py
+++ pyPass.py
@@ -3,26 +3,8 @@ generates unique passwords that are easily reproducible
 
 '''
 
-from subprocess import call, Popen, PIPE
 import hashlib
 import getpass
-
-
-def getUUID():
-    uuid1 = call(["powershell", "get-wmiobject Win32_ComputerSystemProduct |\
-        Select-Object -ExpandProperty UUID"])
-
-    uuid2 = Popen(["powershell", "get-wmiobject Win32_ComputerSystemProduct |\
-        Select-Object -ExpandProperty UUID"], stdout=PIPE)
-
-    output = uuid2.stdout.read()
-    # splited1 = output.partition('\'')
-    # splited2 = splited1.partition('\n')
-
-    print("Test")
-    print(uuid1)
-    print(uuid2)
-    print(output)
 
 
 def hashSHA1(prm):
