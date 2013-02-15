5c9e69cdd01cca024410e1bf91127fc7c76f7f3f
Nanak Tattyrek <nanak@tattyrek.at>
2013-02-15 20:08:36 +0100

    changed indentation to spaces, satisfied SublimeLinter

--- pyPass.py
+++ pyPass.py
@@ -12,23 +12,23 @@ DIGITS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/-_@.:,
 
 
 def hashSHA1(param):
-	# hash parameter with sha1 to hex encoded sting
-	return hashlib.sha1(param.encode('utf-8')).hexdigest()
+    # hash parameter with sha1 to hex encoded sting
+    return hashlib.sha1(param.encode('utf-8')).hexdigest()
 
 
 def inp():
-	keyw = str(input("Enter Keyword: "))
-	while 1:
-		print("Enter Master Password")
-		master1 = str(getpass.getpass())
-		print("Enter Master Password again")
-		master2 = str(getpass.getpass())
-		if master1 == master2:
-			key_and_master = keyw + master1
-			break
-		else:
-			print("Passwords did not match, try again")
-	return key_and_master
+    keyw = str(input("Enter Keyword: "))
+    while 1:
+        print("Enter Master Password")
+        master1 = str(getpass.getpass())
+        print("Enter Master Password again")
+        master2 = str(getpass.getpass())
+        if master1 == master2:
+            key_and_master = keyw + master1
+            break
+        else:
+            print("Passwords did not match, try again")
+    return key_and_master
 
 passw = hashSHA1(inp())
 passw = tran.transcode(passw, DIGITS, tran.BASE16_DIGITS)
